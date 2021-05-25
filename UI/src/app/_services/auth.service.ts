de92ccca1b3062e03a6c68a1fde034048dfa7f62
Hannes Rüger <hannesrueger@gmx.de>
2021-05-25 21:32:48 +0200

    added authentication

---
+++ UI/src/app/_services/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { SocketService } from './socket.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _isProducer = false;
+  private _isAdmin = false;
+  constructor(private socketService: SocketService) { }
+
+  public isProducer() {
+    return this._isProducer;
+  }
+  
+  public isAdmin() {
+    return this._isAdmin;
+  }
+
+  public login(type: "producer" | "settings", username: string, password: string) {
+    return new Promise((resolve) => {
+      this.socketService.socket.emit("login", type, username, password);
+      this.socketService.socket.once("login_result", (result) => {
+        if (result === true) {
+          if (type == "producer") {
+            this._isProducer = true;
+          } else {
+            this._isAdmin = true;
+          }
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      })
+    })
+  }
+}
