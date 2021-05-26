752239905023e96b4e56aeece32e07debe130975
Hannes Rüger <hannesrueger@gmx.de>
2021-05-26 10:28:09 +0200

    added wake lock

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WakeLockService } from './_services/wake-lock.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public showMenu = false;
+  constructor(private wakeLockService: WakeLockService) {
+    wakeLockService.init();
+  }
 }
