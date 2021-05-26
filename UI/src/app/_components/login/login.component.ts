4e240c3daaff0dcc5bd2c2b37ea1f6fa3775f6fb
Hannes Rüger <hannesrueger@gmx.de>
2021-05-26 10:22:16 +0200

    login with enter key

--- UI/src/app/_components/login/login.component.ts
+++ UI/src/app/_components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/_services/auth.service';
 
@@ -13,6 +13,7 @@ export class LoginComponent {
   public username = "";
   public password = "";
   private type!: "producer" | "settings";
+  @ViewChild("inputPassword") public inputPassword!: ElementRef;
 
   constructor(private router: Router, private authService: AuthService) {
   }
