f2bd6901c08c3b941040639f1e3e33b20be9dd16
Hannes Rüger <hannesrueger@gmx.de>
2021-05-26 11:41:21 +0200

    added electron version

---
+++ main.js
@@ -0,0 +1,30 @@
+// This is the electron startup script
+const { app, BrowserWindow } = require('electron');
+require("./index");
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minHeight: 850,
+        minWidth: 1260,
+        webPreferences: {},
+        show: false,
+    });
+    mainWindow.setMenu(null);
+    mainWindow.loadURL('http://127.0.0.1:4455');
+    mainWindow.webContents.on('did-finish-load', function() {
+        mainWindow.show();
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', function() {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') app.quit();
+});
