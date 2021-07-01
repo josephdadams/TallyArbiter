3beed424992078d0cb565a5a751924ae6a759f76
Hannes Rüger <hannesrueger@gmx.de>
2021-07-01 14:13:03 +0200

    some minor changes

--- UI/src/app/_components/home/home.component.ts
+++ UI/src/app/_components/home/home.component.ts
@@ -7,6 +7,7 @@ import { SocketService } from 'src/app/_services/socket.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  public net_interface_url: string = "http://localhost:4455/#/tally";
+  public localNetInterfaceUrl = "http://localhost:4455/#/tally"
+  public netInterfaceUrl = this.localNetInterfaceUrl;
   constructor(public socketService: SocketService) {}
 }
