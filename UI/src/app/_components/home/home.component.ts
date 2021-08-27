e6c870e14fb776a1ada98cc6d999d548590e50d3
Matteo Gheza <matteo.gheza07@gmail.com>
2021-08-27 23:57:49 +0200

    Update home.component.ts

--- UI/src/app/_components/home/home.component.ts
+++ UI/src/app/_components/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { SocketService } from 'src/app/_services/socket.service';
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
