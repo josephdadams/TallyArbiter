a6b254e18ca68790bf20892eb2835fb0b1149a12
Matteo Gheza <matteo.gheza07@gmail.com>
2021-08-31 00:29:02 +0200

    Truncate logs and bug title

--- UI/src/app/_components/error-report/error-report.component.ts
+++ UI/src/app/_components/error-report/error-report.component.ts
@@ -41,6 +41,32 @@ export class ErrorReportComponent implements OnInit, OnDestroy, AfterViewInit {
     );
   }
 
+  generateBugReportUrl(bugTitle: string, version: string, config: object, logs: string, stacktrace: string, runningRecursive: boolean = false): string {
+    
+    //https://medium.com/@DylanAttal/truncate-a-string-in-javascript-41f33171d5a8
+    function truncateString(str: string, num: number) {
+      // If the length of str is less than or equal to num
+      // just return str--don't truncate it.
+      if (str.length <= num) {
+        return str
+      }
+      // Return str truncated with '...' concatenated to the end of str.
+      return str.slice(0, num) + '...'
+    }
+
+    let bugReportUrl: string = `https://github.com/josephdadams/TallyArbiter/issues/new?labels=bug&template=bug.yaml&title=${encodeURIComponent(bugTitle)}&version=${version}&config=${encodeURIComponent(JSON.stringify(config, null, 2))}&logs=${encodeURIComponent(logs)}&stacktrace=${encodeURIComponent(stacktrace)}`;
+    if(bugReportUrl.length > 8190 && !runningRecursive) {
+      bugTitle = truncateString(bugTitle, 60);
+      let logs_split: string[] = logs.split('\n');
+      if(logs_split.length > 10){
+        logs_split.splice(0,logs_split.length-5);
+        logs = logs_split.join('\n');
+      }
+      bugReportUrl = this.generateBugReportUrl(bugTitle, version, config, logs, stacktrace, true);
+    }
+    return bugReportUrl;
+  }
+
   ngOnInit() {
     this.socketService.getErrorReportById(this.currentReportId)
       .then((errorReport) => {
@@ -48,7 +74,7 @@ export class ErrorReportComponent implements OnInit, OnDestroy, AfterViewInit {
         this.loading = false;
         this.validReport = true;
         let bugTitle = "[Bug] "+this.currentReport.stacktrace.split("\n")[0];
-        this.bugReportUrl = `https://github.com/josephdadams/TallyArbiter/issues/new?labels=bug&template=bug.yaml&title=${encodeURIComponent(bugTitle)}&version=${this.socketService.version}&config=${encodeURIComponent(JSON.stringify(this.currentReport.config, null, 2))}&logs=${encodeURIComponent(this.currentReport.logs)}&stacktrace=${encodeURIComponent(this.currentReport.stacktrace)}`;
+        this.bugReportUrl = this.generateBugReportUrl(bugTitle, this.socketService.version as string, this.currentReport.config, this.currentReport.logs, this.currentReport.stacktrace);
         console.log("Error report found:");
         console.log(errorReport);
       })
