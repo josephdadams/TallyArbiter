1c3de09bd49f8544d7734513b8c2d65b5b9ab201
Matteo Gheza <matteo.gheza07@gmail.com>
2021-08-31 15:37:21 +0200

    Add "(unread)" near unreaded error reports

--- UI/src/app/_components/error-reports-list/error-reports-list.component.ts
+++ UI/src/app/_components/error-reports-list/error-reports-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { SocketService } from 'src/app/_services/socket.service';
+import { ErrorReportsListElement } from 'src/app/_models/ErrorReportsListElement';
 
 @Component({
   selector: 'app-error-reports-list',
@@ -8,12 +9,22 @@ import { SocketService } from 'src/app/_services/socket.service';
   styleUrls: ['./error-reports-list.component.scss']
 })
 export class ErrorReportsListComponent implements OnInit {
+  public unreaded_error_reports: any = [];
+  public errorReportsLoaded: boolean = false;
+
   constructor(
     private router: Router,
     public socketService: SocketService,
   ) {
     console.log(this.socketService.errorReports);
-    
+    this.socketService.socket.on('unreaded_error_reports', (list) => {
+      console.log(list);
+      list.forEach((report: ErrorReportsListElement) => {
+        this.unreaded_error_reports.push(report.id);
+      });
+      this.errorReportsLoaded = true;
+    });
+    this.socketService.socket.emit('get_unreaded_error_reports');
   }
 
   public selectErrorReport(id: any) {
