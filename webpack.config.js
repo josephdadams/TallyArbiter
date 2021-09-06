bc916c78530fc702037c8fa5056724484bbdc8de
MatteoGheza <matteo.gheza07@gmail.com>
2021-09-06 13:17:24 +0000

    Add multiple entrypoints in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,54 @@
 const path = require('path');
+const glob = require('glob');
+const TerserPlugin = require("terser-webpack-plugin");
+
+const sources_files = glob.sync('./src/sources/**.ts').reduce(function(obj, el){
+    obj[path.parse(el).name] = el;
+    return obj
+ },{});
+
+ console.log({
+    'main': './src/index.ts',
+    ...sources_files
+});
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: {
+        'main': './src/index.ts',
+        ...sources_files
+    },
     mode: 'production',
     module: {
-      rules: [
-        {
-          test: /\.ts?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
+        rules: [
+          {
+            test: /\.ts?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
     },
     resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.tsx', '.ts', '.js'],
     },
     target: 'node',
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: (pathData) => {
+            return pathData.chunk.name === 'main' ? 'bundle.js' : 'sources/[name].js';
+        },
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
     },
 };
