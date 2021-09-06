1ea90915359107ae01e7effc2b94a0590fa52cae
H. Rüger <hannesrueger@gmx.de>
2021-09-06 21:53:44 +0200

    migrate remaining actions

--- src/actions/TCP.ts
+++ src/actions/TCP.ts
@@ -1,6 +1,7 @@
 import { logger } from "..";
 import { RegisterAction } from "../_decorators/RegisterAction";
 import { Action } from "./_Action";
+import net from "net";
 
 @RegisterAction("79e3ce27", "Generic TCP", [
     { fieldName: 'ip', fieldLabel: 'IP Address', fieldType: 'text' },
@@ -10,6 +11,23 @@ import { Action } from "./_Action";
 ])
 export class TCP extends Action {
     public run(): void {
-        //
+        try {
+            let tcpClient = new net.Socket();
+            tcpClient.connect(this.action.data.port, this.action.data.ip);
+    
+            tcpClient.on('connect', () =>  {
+                let sendBuf = Buffer.from(unescape(this.action.data.string) + this.action.data.end, 'latin1');
+                tcpClient.write(sendBuf);
+                tcpClient.end();
+                tcpClient.destroy(); // kill client after sending data
+                logger(`Generic TCP sent: ${this.action.data.ip}:${this.action.data.port} : ${this.action.data.string}`, 'info');
+            });
+    
+            tcpClient.on('error', (error) => {
+                logger(`An error occured sending the Generic TCP: ${error}`, 'error');
+            });
+        } catch (error) {
+            logger(`An error occured sending the Generic TCP: ${error}`, 'error');
+        }
     }
 }
--- src/actions/TSL.ts
+++ src/actions/TSL.ts
@@ -2,6 +2,8 @@ import { logger } from "..";
 import { RegisterAction } from "../_decorators/RegisterAction";
 import { TallyInputConfigField } from "../_types/TallyInputConfigField";
 import { Action } from "./_Action";
+import dgram from 'dgram';
+import net from 'net';
 
 
 const TSLFields: TallyInputConfigField[] = [ //TSL 3.1 UDP
@@ -20,6 +22,52 @@ const TSLFields: TallyInputConfigField[] = [ //TSL 3.1 UDP
 @RegisterAction("276a8dcc", "TSL 3.1 TCP", TSLFields)
 export class TSL extends Action {
     public run(): void {
-        //
+        try {
+            let bufUMD = Buffer.alloc(18, 0); //ignores spec and pad with 0 for better aligning on Decimator etc
+            bufUMD[0] = 0x80 + parseInt(this.action.data.address);
+            bufUMD.write(this.action.data.label, 2);
+    
+            let bufTally = 0x30;
+    
+            if (this.action.data.tally1) {
+                bufTally |= 1;
+            }
+            if (this.action.data.tally2) {
+                bufTally |= 2;
+            }
+            if (this.action.data.tally3) {
+                bufTally |= 4;
+            }
+            if (this.action.data.tally4) {
+                bufTally |= 8;
+            }
+            bufUMD[1] = bufTally;
+    
+            if (this.action.outputTypeId == "7dcd66b5") {
+                // UDP
+                let client = dgram.createSocket('udp4');
+                client.send(bufUMD, this.action.data.port, this.action.data.ip, function(error) {
+                    if (!error) {
+                        logger(`TSL 3.1 UDP Data sent.`, 'info');
+                    }
+                    client.close();
+                });
+            } else {
+                // TCP
+                let client = new net.Socket();
+                client.connect(this.action.data.port, this.action.data.ip, () =>  {
+                    client.write(bufUMD);
+                });
+        
+                client.on('data', () => {
+                    client.destroy(); // kill client after server's response
+                });
+        
+                client.on('close', () =>  {
+                });
+            }
+        } catch (error) {
+            logger(`An error occured sending the TCP 3.1 ${this.action.outputTypeId == "7dcd66b5" ? "UDP" : "TCP"} Message: ${error}`, 'error');
+        }
     }
 }
