e1eb77c6b8cec68d0d936577668c112f994e3a3c
MatteoGheza <matteo.gheza07@gmail.com>
2021-09-06 20:45:18 +0000

    Add Config model

---
+++ src/_models/Config.ts
@@ -0,0 +1,21 @@
+import { BusOption } from "./BusOption";
+import { CloudDestination } from "./CloudDestination";
+import { ConfigSecuritySection } from "./ConfigSecuritySection";
+import { ConfigTSLClient } from "./ConfigTSLClient";
+import { Device } from "./Device";
+import { DeviceAction } from "./DeviceAction";
+import { DeviceSource } from "./DeviceSource";
+import { Source } from "./Source";
+
+export interface Config {
+    security: ConfigSecuritySection;
+    sources: Source[];
+    devices: Device[];
+    device_sources: DeviceSource[];
+    device_actions: DeviceAction[];
+    tsl_clients: ConfigTSLClient[];
+    tsl_clients_1secupdate: boolean;
+    cloud_destinations: CloudDestination[];
+    cloud_keys: string[];
+    bus_options: BusOption[];
+}
