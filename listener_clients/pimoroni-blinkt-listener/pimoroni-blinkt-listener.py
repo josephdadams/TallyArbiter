43daa6be185f18191fd1e2ca518992967f89f5ae
Matteo Gheza <matteo.gheza07@gmail.com>
2021-10-06 23:08:06 +0200

    Update pimoroni blinkt listener to support v3.0.0

--- listener_clients/pimoroni-blinkt-listener/pimoroni-blinkt-listener.py
+++ listener_clients/pimoroni-blinkt-listener/pimoroni-blinkt-listener.py
@@ -9,72 +9,90 @@
 from signal import signal, SIGINT
 from sys import exit
 import sys
+import os
 import time
+from uuid import uuid4
+import socketio
+import argparse
+import configparser
+
+config = configparser.ConfigParser()
+if not os.path.isfile('config.ini'):
+	deviceId = ''
+	if os.path.isfile('deviceid.txt'):
+		print("pimoroni-blinkt-listener has been updated since the last time you used it.")
+		print("Please read the new documentation to use this program property.")
+		print("Reading device ID from deviceid.txt")
+		deviceId = open('deviceid.txt', 'r').read()
+	config['DEFAULT'] = {
+		'deviceId': deviceId,
+		'host': 'localhost',
+		'port': 4455,
+		'clientUUID': uuid4()
+	}
+	with open('config.ini', 'w') as configfile:
+		config.write(configfile)
+else:
+	config.read('config.ini')
+
+if os.path.isfile('deviceid.txt'):
+	os.remove('deviceid.txt')
+
+parser = argparse.ArgumentParser(description='Tally Arbiter Pimoroni Blinkt Listener')
+parser.add_argument('--host', default=config['DEFAULT']['host'], help='Hostname or IP address of the server')
+parser.add_argument('--port', default=config['DEFAULT']['port'], help='Port of the server')
+parser.add_argument('--device-id', default=None, help='Load with custom device id')
+parser.add_argument('--debug', action='store_true', help='Show advanced logs usefull for debugging')
+parser.add_argument('--disable-reassign', action='store_true', help='Disable device reassignmend from UI')
+parser.add_argument('--disable-flash', action='store_true', help='Disable client listener flash')
+parser.add_argument('--skip-blinkt', action='store_true', help='Skip the Blinkt! requirment and simulate it (only for debugging)')
+args = parser.parse_args()
+
+def debug(message = None):
+	if args.debug:
+		if message:
+			print(message)
+		else:
+			print()
+
+debug(args)
+
 try:
-    import blinkt
+    import blinkt # pyright: reportMissingImports=false
+    try:
+        blinkt.set_clear_on_exit(True)
+    except:
+        pass
 except ImportError:
     blinkt = None
-import socketio
-import json
+    if not args.skip_blinkt:
+        print("Blinkt is not installed. Please install it and try again.")
+        print("If you want to try this program simulating Blinkt! add the flag --skip-blinkt")
+        exit(1)
 
 device_states = []
 bus_options = []
-mode_preview = False
-mode_program = False
-
-if len(sys.argv) == 0:
-	server = sys.argv[1]
-else:
-	server = 'localhost'
-
-stored_deviceId = ''
-
-try:
-	blinkt.set_clear_on_exit(True)
-except:
-	pass
-
 debounce = False #used to keep calls from happing concurrently
 
-try:
-	stored_deviceId_file = open('deviceid.txt')
-	stored_deviceId = stored_deviceId_file.read()
-	stored_deviceId_file.close()
-except IOError:
-	stored_deviceId = ''
+if config['DEFAULT']['deviceId']:
+	print('Last Used Device Id: ' + config['DEFAULT']['deviceId'])
 
-print('Last Used Device Id: ' + stored_deviceId)
-
-if len(sys.argv) > 2:
-	port = sys.argv[2]
-else:
-	port = '4455'
-
-if len(sys.argv) > 3:
-	deviceId = sys.argv[3]
-else:
-	if (stored_deviceId != ''):
-		deviceId = stored_deviceId
-	else:
-		deviceId = 'null'
-
-#close program if ctrl+c is pressed
-def signal_handler(sig, frame):
-	exit(0)
-
-signal(SIGINT, signal_handler)
+if not args.device_id:
+	if not config['DEFAULT']['deviceId']:
+		config['DEFAULT']['deviceId'] = 'null'
+	args.device_id = config['DEFAULT']['deviceId']
 
 #SocketIO Connections
 sio = socketio.Client()
 
 @sio.event
 def connect():
-	print('Connected to Tally Arbiter server:', server, port)
+	print('Connected to Tally Arbiter server:', args.host, args.port)
 	sio.emit('listenerclient_connect', {  # start listening for the device
-		'deviceId': deviceId,
-		'listenerType': 'blinkt_test',
-		'canBeReassigned': True,
-		'canBeFlashed': True,
+		'deviceId': config['DEFAULT']['deviceId'],
+		'listenerType': 'pimoroni_blinkt_' + config['DEFAULT']['clientUUID'],
+		'canBeReassigned': not args.disable_reassign,
+		'canBeFlashed': not args.disable_flash,
 		'supportsChat': False
 	})
 	repeatNumber = 2
@@ -87,7 +105,7 @@ def connect():
 
 @sio.event
 def connect_error(data):
-	print('Unable to connect to Tally Arbiter server:', server, port)
+	print('Unable to connect to Tally Arbiter server:', args.host, args.port)
 	doBlink(150, 150, 150)
 	time.sleep(.3)
 	doBlink(0, 0, 0)
@@ -95,7 +113,7 @@ def connect_error(data):
 
 @sio.event
 def disconnect():
-	print('Disconnected from Tally Arbiter server:', server, port)
+	print('Disconnected from Tally Arbiter server:', args.host, args.port)
 	doBlink(255, 255, 255)
 	time.sleep(.3)
 	doBlink(0, 0, 0)
@@ -103,7 +121,7 @@ def disconnect():
 
 @sio.event
 def reconnect():
-	print('Reconnected to Tally Arbiter server:', server, port)
+	print('Reconnected to Tally Arbiter server:', args.host, args.port)
 	repeatNumber = 2
 	while(repeatNumber):
 		repeatNumber = repeatNumber - 1
@@ -111,6 +129,10 @@ def reconnect():
 		time.sleep(.3)
 		doBlink(0, 0, 0)
 		time.sleep(.3)
+
+@sio.on('error')
+def on_error(error):
+	print(error)
 
 @sio.on('device_states')
 def on_device_states(data):
@@ -121,10 +143,13 @@ def on_device_states(data):
 @sio.on('bus_options')
 def on_bus_options(data):
 	global bus_options
+	debug(data)
 	bus_options = data
 
 @sio.on('flash')
 def on_flash():
+	if args.disable_flash:
+		return
 	doBlink(255, 255, 255)
 	time.sleep(.5)
 	doBlink(0, 0, 0)
@@ -135,10 +160,11 @@ def on_flash():
 	time.sleep(.5)
 	doBlink(255, 255, 255)
 	time.sleep(.5)
-	evaluateMode()
 
 @sio.on('reassign')
 def on_reassign(oldDeviceId, newDeviceId):
+	if args.disable_reassign:
+		return
 	print('Reassigning from DeviceID: ' + oldDeviceId + ' to Device ID: ' + newDeviceId)
 	doBlink(0, 0, 0)
 	time.sleep(.1)
@@ -150,60 +176,64 @@ def on_reassign(oldDeviceId, newDeviceId):
 	time.sleep(.1)
 	doBlink(0, 0, 0)
 	sio.emit('listener_reassign', data=(oldDeviceId, newDeviceId))
-	global deviceId
-	deviceId = newDeviceId
-	stored_deviceId_file = open('deviceid.txt', 'w')
-	stored_deviceId_file.write(newDeviceId)
-	stored_deviceId_file.close()
+	config['DEFAULT']['deviceId'] = newDeviceId
+	with open('config.ini', 'w') as configfile:
+		config.write(configfile)
 
-def getBusTypeById(busId):
+def getBusById(busId):
 	for bus in bus_options:
 		if bus['id'] == busId:
-			return bus['type']
+			return bus
+
+def hex_to_rgb(hex_string):
+	hex_string = hex_string.lstrip('#')
+	return tuple(int(hex_string[i:i+2], 16) for i in (0, 2, 4))
 
 def processTallyData():
-	global mode_preview
-	global mode_program
+	busses_list = []
 	for device_state in device_states:
-		if getBusTypeById(device_state['busId']) == 'preview':
-			if len(device_state['sources']) > 0:
-				mode_preview = True
-			else:
-				mode_preview = False
-		elif getBusTypeById(device_state['busId']) == 'program':
-			if len(device_state['sources']) > 0:
-				mode_program = True
-			else:
-				mode_program = False
-	evaluateMode()
-
-def evaluateMode():
-	if (mode_preview == True) and (mode_program == False):		# preview mode, color it green
-		doBlink(0, 255, 0)
-	elif (mode_preview == False) and (mode_program == True):	# program mode, color it red
-		doBlink(255, 0, 0)
-	elif (mode_preview == True) and (mode_program == True):		# preview+program mode, color it yellow
-		doBlink(255, 255, 0)
-	else:														# no source, turn it off
-		doBlink(0, 0, 0)
+		current_bus = getBusById(device_state['busId'])
+		if len(device_state['sources']) > 0:
+			busses_list.append(current_bus)
+	
+	if len(busses_list) == 0:
+		debug("Bus: nothing")
+		doBlink(0,0,0)
+	else:
+		try:
+			busses_list.sort(key=lambda x: x['priority'])
+		except:
+			return
+		#TODO: make this work with more than one bus
+		'''
+		for current_bus in busses_list:
+			current_color = hex_to_rgb(current_bus['color'])
+			debug('Bus: ' + current_bus['type'] + ' - ' + ";".join(str(n) for n in current_color))
+			doBlink(*current_color)
+		'''
+		current_color = hex_to_rgb(busses_list[0]['color'])
+		debug('Bus: ' + busses_list[0]['type'] + ' - ' + ";".join(str(n) for n in current_color))
+		doBlink(*current_color)
+	debug()
 
 def doBlink(r, g, b):
-	global debounce
+	global debounce, args
 	if (debounce != True):
 		debounce = True
 		if blinkt:
 			blinkt.set_all(r, g, b)
 			blinkt.show()
-		else:
-			print("\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, "Blink"), flush=True, end='\r')
+		elif args.skip_blinkt:
+			print("\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, " Blinkt! color"))
 		debounce = False
 
 while(1):
 	try:
-		sio.connect('http://' + server + ':' + port)
+		sio.connect('http://' + args.host + ':' + args.port)
 		sio.wait()
 		print('Tally Arbiter Listener Running. Press CTRL-C to exit.')
-		print('Attempting to connect to Tally Arbiter server: ' + server + '(' + port + ')')
+		print('Attempting to connect to Tally Arbiter server: ' + args.host + '(' + args.port + ')')
+		print()
 	except KeyboardInterrupt:
 		print('Exiting Tally Arbiter Listener.')
 		doBlink(0, 0, 0)
