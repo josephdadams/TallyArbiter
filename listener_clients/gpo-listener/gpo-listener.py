36a643ece6da69b5a5eaa411f23c94ffe914c959
Matteo Gheza <matteo.gheza07@gmail.com>
2021-10-07 16:08:49 +0200

    Update GPO listener to support v3.0.0

--- listener_clients/gpo-listener/gpo-listener.py
+++ listener_clients/gpo-listener/gpo-listener.py
@@ -5,22 +5,86 @@
 from signal import signal, SIGINT
 from sys import exit
 import sys
+import os
+import shutil
 import time
+from uuid import uuid4
 import socketio
 import json
 import atexit
-import RPi.GPIO as GPIO
+import argparse
 
-if len(sys.argv) > 1:
-	configFileName = sys.argv[1]
-else:
-	configFileName = 'config_gpo.json'
+parser = argparse.ArgumentParser(description='Tally Arbiter Blink(1) Listener')
+parser.add_argument('--ip', help='Hostname or IP address of the server')
+parser.add_argument('--port', help='Port of the server')
+parser.add_argument('--config', default='config_gpo.json', help='Load with custom device id')
+parser.add_argument('--disable-reassign', action='store_true', help='Disable device reassignmend from UI')
+parser.add_argument('--disable-flash', action='store_true', help='Disable client listener flash')
+parser.add_argument('--skip-gpio', action='store_true', help='Skip the Rpi.GPIO requirment and simulate it (only for debugging)')
+args = parser.parse_args()
+
+configFileName = args.config
 
 server_config = []
 gpo_groups = []
 
 device_states = []
 bus_options = []
+
+try:
+	if configFileName == 'config_gpo.json' and not os.path.isfile('config_gpo.json'):
+		shutil.copyfile(configFileName + '.example', configFileName)
+		print('config_gpo.json does not exist, creating a new one.')
+	
+	config_file = open(configFileName)
+	config = config_file.read()
+	config_file.close()
+	if (config != ''):
+		configJson = json.loads(config)
+		if not 'clientUUID' in configJson:
+			configJson['clientUUID'] = str(uuid4())
+			with open('config_gpo.json', 'w') as outfile:
+				json.dump(configJson, outfile, indent=4)
+		server_config = configJson['server_config']
+		gpo_groups = configJson['gpo_groups']
+	else:
+		print('Config data could not be loaded.')
+		exit (0)
+except IOError:
+	print('Config file could not be located.')
+	exit (0)
+
+if args.ip:
+	server_config['ip'] = args.ip
+
+if args.port:
+	server_config['port'] = args.port
+
+class GPIOsimulator:
+	BCM = "BCM"
+	OUT = "OUT"
+
+	def setmode(self, mode):
+		print('Setting GPIO mode to {}'.format(mode))
+	
+	def setwarnings(self, state):
+		print('Setting GPIO warnings to {}'.format(state))
+
+	def setup(self, pin, mode):
+		print('Setting GPIO pin {} to mode {}'.format(pin, mode))
+	
+	def output(self, pin, state):
+		pass
+		#print('Setting GPIO pin {} to state {}'.format(pin, state))
+
+try:
+	import RPi.GPIO as GPIO # pyright: reportMissingImports=false
+except ImportError:
+	GPIO = GPIOsimulator()
+	if not args.skip_gpio:
+		print("Rpi.GPIO is not found. Please install it and try again.")
+		print("If you want to try this program simulating GPIO add the flag --skip-gpio")
+		exit(1)
 
 def setStates():
 	GPIO.setmode(GPIO.BCM)
@@ -51,7 +115,13 @@ def connect():
 	sio.emit('bus_options')
 	setStates()
 	for gpo_group in gpo_groups:
-		sio.emit('device_listen_gpo', {'gpoGroupId': gpo_group['id'], 'deviceId': gpo_group['deviceId']})
+		sio.emit('listenerclient_connect', {  # start listening for the device
+			'deviceId': gpo_group['deviceId'],
+			'listenerType': 'gpo_' + configJson['clientUUID'] + "_" + gpo_group['id'],
+			'canBeReassigned': not args.disable_reassign,
+			'canBeFlashed': not args.disable_flash,
+			'supportsChat': False
+		})
 
 @sio.event
 def connect_error(data):
@@ -64,6 +134,10 @@ def disconnect():
 @sio.event
 def reconnect():
 	print('Reconnected to Tally Arbiter server:', server_config['ip'], server_config['port'])
+
+@sio.on('error')
+def on_error(error):
+	print(error)
 
 @sio.on('device_states')
 def on_device_states(data):
@@ -78,6 +152,8 @@ def on_bus_options(data):
 
 @sio.on('flash')
 def on_flash(gpoGroupId):
+	if args.disable_flash:
+		return
 	for gpo_group in gpo_groups:
 		if gpo_group['id'] == gpoGroupId:
 			for gpo in gpo_group['gpos']:
@@ -92,11 +168,12 @@ def on_flash(gpoGroupId):
 				GPIO.output(gpo['pinNumber'], gpo['state'])
 
 @sio.on('reassign')
-def on_reassign(gpoGroupId, oldDeviceId, newDeviceId):
-	print('Reassigning GPO Group ' + gpoGroupId + ' from DeviceID: ' + oldDeviceId + ' to Device ID: ' + newDeviceId)
-	sio.emit('listener_reassign_gpo', data=(gpoGroupId, oldDeviceId, newDeviceId))
+def on_reassign(oldDeviceId, newDeviceId):
+	if args.disable_reassign:
+		return
+	print('Reassigning GPO Group from DeviceID: ' + oldDeviceId + ' to Device ID: ' + newDeviceId)
 	for gpo_group in gpo_groups:
-		if gpo_group['id'] == gpoGroupId:
+		if gpo_group['deviceId'] == oldDeviceId:
 			gpo_group['deviceId'] = newDeviceId
 
 	config_file = open(configFileName, 'w')
@@ -143,22 +220,6 @@ def UpdateGPO(deviceId, bus, value):
 						print('Taking GPIO Pin {} Low'.format(gpo['pinNumber']))
 						GPIO.output(gpo['pinNumber'], False)
 						gpo['state'] = False
-
-try:
-	config_file = open(configFileName)
-	config = config_file.read()
-	config_file.close()
-	if (config != ''):
-		configJson = json.loads(config)
-		server_config = configJson['server_config']
-		gpo_groups = configJson['gpo_groups']
-	else:
-		print('Config data could not be loaded.')
-		exit (0)
-except IOError:
-	print('Config file could not be located.')
-	exit (0)
-
 
 while(1):
 	try:
