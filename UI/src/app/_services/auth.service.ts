2748be8d3feebf3bcd60b5d9adecbf8eeee080f3
Matteo Gheza <matteo.gheza07@gmail.com>
2021-10-20 22:06:30 +0200

    Add roles

--- UI/src/app/_services/auth.service.ts
+++ UI/src/app/_services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SocketService } from './socket.service';
+import jwt_decode from 'jwt-decode';
 
 export interface LoginResponse {
   loginOk: boolean;
@@ -13,7 +14,45 @@ export interface LoginResponse {
 export class AuthService {
   private _isProducer = false;
   private _isAdmin = false;
-  constructor(private socketService: SocketService) { }
+
+  public access_token: string = "";
+  public profile: any = undefined;
+
+  constructor(private socketService: SocketService) {
+    if(localStorage.getItem("access_token") !== null) {
+      this.access_token = localStorage.getItem("access_token") as string;
+      this.loadProfile();
+      this.socketService.sendAccessToken(this.access_token);
+    }
+  }
+
+  private setToken(value: string) {
+    localStorage.setItem("access_token", value);
+    this.access_token = value;
+    this.loadProfile();
+    this.socketService.sendAccessToken(this.access_token);
+  }
+
+  private removeToken() {
+    localStorage.removeItem("access_token");
+    this.access_token = "";
+  }
+
+  public loadProfile() {
+    console.log("Loading profile", this.access_token);
+    let now = Date.now().valueOf() / 1000;
+    let decoded: any = jwt_decode(this.access_token);
+    if (typeof decoded.exp !== 'undefined' && decoded.exp < now) {
+      return false;
+    }
+    if (typeof decoded.nbf !== 'undefined' && decoded.nbf > now) {
+      return false;
+    }
+    this.profile = decoded.user;
+
+    console.log(this.profile);
+    return true;
+  }
 
   public isProducer() {
     return this._isProducer;
@@ -28,6 +67,7 @@ export class AuthService {
       this.socketService.socket.emit("login", type, username, password);
       this.socketService.socket.once("login_response", (response: LoginResponse) => {
         if (response.loginOk === true) {
+          this.setToken(response.accessToken);
           if (type == "producer") {
             this._isProducer = true;
           } else {
@@ -37,5 +77,12 @@ export class AuthService {
         resolve(response);
       })
     })
+  }
+
+  public logout() {
+    this.removeToken();
+    this._isProducer = false;
+    this._isAdmin = false;
+    this.profile = undefined;
   }
 }
