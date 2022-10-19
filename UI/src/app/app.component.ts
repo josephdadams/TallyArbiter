8a45fc0f99cab211759686812c42ba43d3a62586
Matteo Gheza <matteo.gheza07@gmail.com>
2022-10-20 00:00:29 +0200

    Fix server reconnection, add conn. lost snackbar

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,22 +1,41 @@
 import { Component } from '@angular/core';
 import { WakeLockService } from './_services/wake-lock.service';
 import { NavbarVisibilityService } from './_services/navbar-visibility.service';
+import { connLostSnackbarService } from './_services/conn-lost-snackbar.service';
 import { LocationBackService } from 'src/app/_services/locationBack.service';
 import { DarkModeService } from './_services/darkmode.service';
 import { AuthService } from './_services/auth.service';
 
+import { trigger, transition, style, animate, state } from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fade', [
+      state('hidden', style({
+        opacity: 0
+      })),
+      state('show', style({
+        opacity: 1
+      })),
+      transition('hidden => show', [
+        animate('0.2s')
+      ]),
+      transition('show => hidden', [
+        animate('0.2s')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   public showMenu = false;
-  public hideNavBar = false;
   
   constructor(
     private wakeLockService: WakeLockService,
     public navbarVisibilityService: NavbarVisibilityService,
+    public connLostSnackbar: connLostSnackbarService,
     private locationBackService: LocationBackService,
     public darkModeService: DarkModeService,
     public authService: AuthService
