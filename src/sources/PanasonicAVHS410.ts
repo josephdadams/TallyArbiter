ec4ac86ebb8a707e76eb81eee3520fd7f9610db8
dchadd427 <125092173+dchadd427@users.noreply.github.com>
2024-06-20 08:21:37 -0700

    Says port 60020 is already in use

--- src/sources/PanasonicAVHS410.ts
+++ src/sources/PanasonicAVHS410.ts
@@ -34,9 +34,9 @@ const sourceTypesPanasonic = [
     { id: '23', label: 'XPT 24' },
 ];
 
-const PanasonicAVHS410Port = 60020;
+//const PanasonicAVHS410Port = 60020;
 
-@UsesPort(PanasonicAVHS410Port)
+//@UsesPort(PanasonicAVHS410Port)
 @RegisterTallyInput("7da3b526", "Panasonic AV-HS410", "Uses port 60020. Make sure to have Multicast enabled on the network", [
     { fieldName: 'ip', fieldLabel: 'IP Address', fieldType: 'text' },
 	{ fieldName: 'port', fieldLabel: 'Port', fieldType: 'port' }
