a0f1fcc620404a2549abdf691aea4846324595e9
Joseph Adams <josephdadams@gmail.com>
2025-11-11 22:37:36 -0500

    Improves file loading mechanism
    Updates the file loading process to be more robust by ignoring hidden files, non-JS/TS files, and directories.
    
    This prevents errors caused by attempting to load unexpected file types and improves the overall stability of the application.

--- src/_helpers/fileLoader.ts
+++ src/_helpers/fileLoader.ts
@@ -2,7 +2,18 @@ import fs from 'fs'
 import path from 'path'
 
 export function loadClassesFromFolder(folder: string): void {
-	for (const file of fs.readdirSync(path.join(__dirname, '../', folder)).filter((f) => !f.startsWith('_'))) {
-		require(path.join(__dirname, '../', folder, file.replace('.ts', '')))
+	const dirPath = path.join(__dirname, '../', folder)
+
+	for (const file of fs.readdirSync(dirPath)) {
+		// Skip hidden files, non-JS/TS files, and folders
+		if (
+			file.startsWith('.') ||                    // ignore .DS_Store, etc.
+			!file.match(/\.(js|ts)$/) ||               // ignore non-code files
+			fs.statSync(path.join(dirPath, file)).isDirectory() // skip folders
+		) {
+			continue
+		}
+
+		require(path.join(dirPath, file.replace(/\.(ts|js)$/, '')))
 	}
 }
